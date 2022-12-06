cc64e651d940a73209c44bc0a823a71a5f2f6e2f
blaxxun <blaxxun@blaxxun.blaxxun>
2022-12-06 21:25:04 +0100

    mistlands recompile

--- ConversionSizeSpeed/ConversionSizeSpeed.cs
+++ ConversionSizeSpeed/ConversionSizeSpeed.cs
@@ -16,7 +16,7 @@ namespace ConversionSizeSpeed;
 public class ConversionSizeSpeed : BaseUnityPlugin
 {
 	private const string ModName = "Conversion Size & Speed";
-	private const string ModVersion = "1.0.6";
+	private const string ModVersion = "1.0.7";
 	private const string ModGUID = "org.bepinex.plugins.conversionsizespeed";
 
 	private readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
