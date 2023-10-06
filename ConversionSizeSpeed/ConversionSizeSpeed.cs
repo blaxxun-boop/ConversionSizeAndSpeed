2b6d60fe8dfc2fd1bd33f8a39ac0b7f61b4c2519
blaxxun <blaxxun@blaxxun.blaxxun>
2023-10-06 17:48:18 +0200

    fixes for latest valheim update

--- ConversionSizeSpeed/ConversionSizeSpeed.cs
+++ ConversionSizeSpeed/ConversionSizeSpeed.cs
@@ -17,7 +17,7 @@ namespace ConversionSizeSpeed;
 public class ConversionSizeSpeed : BaseUnityPlugin
 {
 	private const string ModName = "Conversion Size & Speed";
-	private const string ModVersion = "1.0.14";
+	private const string ModVersion = "1.0.15";
 	private const string ModGUID = "org.bepinex.plugins.conversionsizespeed";
 
 	private readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
